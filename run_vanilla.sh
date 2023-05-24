e6c48dfeceffcec8f5a23a19f7cddbe21a3d908f
Ricu <vl1102@web.de>
2023-05-24 11:07:35 +0200

    update

--- run_vanilla.sh
+++ run_vanilla.sh
@@ -1,7 +1,8 @@
-declare -a StringArray=("cola" "sst2" "mrpc" "qnli" "rte"  "stsb" "wnli")
-# done: 
-# to big: "mnli" "qqp"
+declare -a StringArray=("mnli")
+# to be done: "qnli" "rte"  "stsb" "wnli"
+# to big:  "qqp"
+# done:"cola" "sst2" "mrpc"
 for task_name in "${StringArray[@]}";
 do
-   python run_glue_no_trainer.py --model_name_or_path bert-base-cased --task_name $task_name --max_length 128 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3  --output_dir /content/mod_runs/$task_name --seed 0
+   python run_glue_no_trainer.py --model_name_or_path bert-base-cased --task_name $task_name --max_length 128 --per_device_train_batch_size 32 --learning_rate 2e-5 --num_train_epochs 3  --output_dir /content/vanilla_runs/$task_name --seed 0
 done
